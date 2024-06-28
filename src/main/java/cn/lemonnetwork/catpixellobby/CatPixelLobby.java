bdb8ea64188cfd0de9cec03610023ee981809338
duduskz <DF12345678SA@outlook.com>
2024-06-28 11:48:58 +0800

    更新说明信息

--- src/main/java/cn/lemonnetwork/catpixellobby/CatPixelLobby.java
+++ src/main/java/cn/lemonnetwork/catpixellobby/CatPixelLobby.java
@@ -17,12 +17,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CatPixelLobby extends JavaPlugin {
 
   //CatPixelLobby Crack 赵泽民反编译大神出品 LemonNetwork 破解
-  //"请！注！意！
+  //请！注！意！
   //此插件属于CatPixelLobby反编译项目，如侵权，LemonNetwork不负任何法律责任
   //该插件的 DeliveryMan 及 Achievement 代码均为作者赵泽民非法侵犯我工作室著作权代码
   //LemonDeliveryMan 在 duduskz 资源库下属于 GPL3.0 开源协议项目
   //LeaderBoard代码为 StarryCountry-Development 作品
   //使用或利用时请考虑版权问题！
+  //本项目同样为 GPL3.0 协议
   //请遵守开源社区精神，我们应该反对赵泽民这种老鼠屎！
   //可笑的是，赵泽民大神的大量写代码习惯全部来源于LemonNetwork，例如 MySQL 连接 去看Config就知道了、类名大写这种低级错误
 
@@ -62,6 +63,7 @@ public class CatPixelLobby extends JavaPlugin {
     Bukkit.getConsoleSender().sendMessage("LemonDeliveryMan 在 duduskz 资源库下属于 GPL3.0 开源协议项目");
     Bukkit.getConsoleSender().sendMessage("LeaderBoard代码为 StarryCountry-Development 作品");
     Bukkit.getConsoleSender().sendMessage("使用或利用时请考虑版权问题！");
+    Bukkit.getConsoleSender().sendMessage("本项目同样为 GPL3.0 协议");
     Bukkit.getConsoleSender().sendMessage("请遵守开源社区精神，我们应该反对赵泽民这种老鼠屎！");
     Bukkit.getConsoleSender().sendMessage("可笑的是，赵泽民大神的大量写代码习惯全部来源于LemonNetwork，例如 MySQL 连接 去看Config就知道了、类名大写这种低级错误");
 
